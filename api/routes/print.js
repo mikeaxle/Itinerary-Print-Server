7c3c3682e0768d7dd70f2280d68486b0a17a65cc
mikeaxle <mikeaxle2006@gmail.com>
2018-05-18 11:37:22 +0200

    so many stuff

--- api/routes/print.js
+++ api/routes/print.js
@@ -1,58 +1,56 @@
-'use strict';
+"use strict";
 
 module.exports = function(app) {
+  // import html-pdf plugin
+  var pdf = require("html-pdf");
 
-    // import html-pdf plugin
-    var pdf = require('html-pdf');
-    
-    // import path
-    var path = require('path');
-    
-    // set base path
-    let base = "file://" + path.resolve(path.join(__dirname, '..', '/views/')) + '/';
-    
-             // pdf document size options
-    	const options = {
-    		base: base,
-        	type: 'pdf',
-        	border: {
-            	"top": "0mm",      
-            	"right": "0mm",
-            	"bottom": "0mm",
-            	"left": "0mm"
-          	},
-          	header: {
-            	height:'35mm',
-            	// contents: header,
-          	},
-          footer: {
-          //  contents: footer,
-          	}
-    	};
-        
+  // import path
+  var path = require("path");
 
-    
-    app.post('/print-pdf', (req,res, done) => {
+  // set base path
+  let base =
+    "file://" + path.resolve(path.join(__dirname, "..", "/views/")) + "/";
 
-        console.log('print started');
+  // pdf document size options
+  const options = {
+    base: base,
+    type: "pdf",
+    border: {
+      top: "0mm",
+      right: "0mm",
+      bottom: "0mm",
+      left: "0mm"
+    },
+    header: {
+      height: "35mm"
+      // contents: header,
+    },
+    footer: {
+      //  contents: footer,
+    }
+  };
 
-        // set cors
-        res.setHeader('Access-Control-Allow-Origin', '*');
-    
-        // set content type and file name
-        res.set('Content-Disposition', 'attachment; filename=itinerary.pdf');
-        res.set('Content-Type', "application/pdf");
-    
-        // html string to print
-        const html = req.body.html;
+  app.post("/print-pdf", (req, res, done) => {
+    console.log("print started");
+    // set cors
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
-        try {
-            pdf.create(html, options).toStream(function(err, stream){
-                stream.pipe(res);
-              });
-        } catch (error) {
-            alert(error);
-        }
-        done;
+    // set content type and file name
+    res.set("Content-Disposition", "attachment; filename=itinerary.pdf");
+    res.set("Content-Type", "application/pdf");
+
+    // html string to print
+    const html = req.body.html;
+    // console.log(pdf)
+
+    try {
+      pdf.create(html, options).toStream(function(err, stream) {
+        stream.pipe(res);
+        // console.log(res)
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    done;
   });
 };
