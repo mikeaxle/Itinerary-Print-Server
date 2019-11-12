da199cf1f457a49dffbb2e348678d547cd9beaff
Saint Michael <mikeaxle2006@gmail.com>
2019-11-12 17:42:25 +0200

    added: Itinerary - Unique URL

---
+++ api/routes/liveUrl.js
@@ -0,0 +1,78 @@
+"use strict";
+
+module.exports = function(app) {
+  // import html-pdf plugin
+  let pdf = require("html-pdf");
+
+  // import path
+  let path = require("path");
+
+  // import basic ftp
+  let ftp = require("basic-ftp")
+
+  // import readable
+  const Readable = require('stream').Readable;
+
+
+  app.post("/liveUrl", async (req, res, done) => {
+
+    let uploadResult;
+
+    console.log("live url started");
+    // set cors
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    // set content type
+    res.set("Content-Type", "text/plain");
+
+    // html string to print
+    let html = req.body.html
+
+    // get title from request
+    let title = req.body.title
+
+    console.log('========== \nhtml content\n=========')
+
+    // create readable
+    const readable = new Readable()
+    
+    // add html to readable
+    readable.push(html)
+
+    // eof
+    readable.push(null)
+
+    // new ftp client
+    let client = new ftp.Client()
+    
+    // // set verbose
+    client.ftp.verbose = true
+    try {
+        // access ftp server
+        await client.access({
+            host: "files.000webhost.com",
+            user: "itinerary-manager-ta",
+            password: "Wolf@1988",
+            secure: false
+        })
+
+        // upload file and get result
+        let result = await client.uploadFrom(readable, `public_html/${title}.html`)
+        console.log(result)
+
+        // upload Result 
+        uploadResult = `https://itinerary-manager-ta.000webhostapp.com/${title}.html`;
+    }
+    catch(err) {
+        // get error
+        console.log(err)
+        uploadResult = err.message
+    }
+
+    // close ftp client
+    client.close()
+    // send result as 
+    res.send(uploadResult)
+    done
+  });
+};
--- server.js
+++ server.js
@@ -9,7 +9,10 @@ var express = require('express'),
   app.use(cors())
   
   var routes = require('./api/routes/print');
+  var routes2 = require('./api/routes/liveUrl');
+
   routes(app); //register the route
+  routes2(app)
 
   app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
